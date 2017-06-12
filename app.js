3e1d48c1648283c057c5cddff608aaae795bc21c
“rogrenke” <roland@rolandgrenke.com>
2017-06-12 18:55:13 +0100

    Daniele fixed the broken POST. Bellissimo :)

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
+
+// var mongoose = require('mongoose');
+// var mongoDB = 'mongodb://localhost/meanDB'
+// mongoose.connect(mongoDB);
+// var db = mongoose.connection;
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
+
 app.set('view engine', 'pug')
-var mongoose = require('mongoose');
-var mongoDB = 'mongodb://localhost/meanDB'
-mongoose.connect(mongoDB);
-var db = mongoose.connection;
-var space = require('./routes/space');
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/spaces', space);
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
-// app.get('/spaces', function (req, res) {
-//   res.render('spaces', {title: 'All available spaces', message: 'select your space!'})
-// })
+app.get('/spaces/new', function(req, res) {
+  res.render('spaces', {title: 'List a space', message: 'list your space!'})
+})
 
-// app.get('/spaces/new', function (req, res) {
-//   res.render('spaces', {title: 'List a space', message: 'list your space!'})
-// })
-
-
+app.post('/spaces', function(req, res) {
+  console.log(req.body)
+  res.redirect('/')
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
